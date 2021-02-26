b75cebaf6199b50e9be0a822373c5338fda94a28
mikelove <michaelisaiahlove@gmail.com>
2021-02-26 13:40:35 -0500

    styler on bootstrap.R

--- R/bootstrap.R
+++ R/bootstrap.R
@@ -3,9 +3,9 @@
 #' @param x the input GRanges
 #' @param type the type of null generation
 #' @param L_b the length of the block
-#' 
+#'
 #' @export
-bootstrap_granges <- function(x, type=c("bootstrap", "permute"), within_chrom=TRUE, L_b) {
+bootstrap_granges <- function(x, type = c("bootstrap", "permute"), within_chrom = TRUE, L_b) {
   type <- match.arg(type)
   chrom_lens <- seqlengths(x)
   tab <- table(seqnames(x))
@@ -15,11 +15,11 @@ bootstrap_granges <- function(x, type=c("bootstrap", "permute"), within_chrom=TR
       L_s <- chrom_lens[[chr]]
       r <- ranges(x[seqnames(x) == chr])
       r_prime <- if (type == "bootstrap") {
-                   bootstrap_iranges(r, L_s, L_b)
-                 } else {
-                   permute_blocks_iranges(r, L_s, L_b)
-                 }
-      GRanges(seqnames=chr, ranges=r_prime, seqlengths=chrom_lens)
+        bootstrap_iranges(r, L_s, L_b)
+      } else {
+        permute_blocks_iranges(r, L_s, L_b)
+      }
+      GRanges(seqnames = chr, ranges = r_prime, seqlengths = chrom_lens)
     })
     x_prime <- do.call(c, res)
   } else {
@@ -27,7 +27,7 @@ bootstrap_granges <- function(x, type=c("bootstrap", "permute"), within_chrom=TR
     # chromosomes to a single long chromosome,
     # then perform block bootstrap / permutation,
     # then map back to the original chromosomes.
-    
+
     # TODO: code assumes sorted 'x'
     stopifnot(all(x == GenomicRanges::sort(x)))
 
@@ -35,10 +35,10 @@ bootstrap_granges <- function(x, type=c("bootstrap", "permute"), within_chrom=TR
     r <- map_chroms_to_line(x)
     L_s <- sum(seqlengths(x))
     r_prime <- if (type == "bootstrap") {
-                 bootstrap_iranges(r, L_s, L_b)
-               } else {
-                 permute_blocks_iranges(r, L_s, L_b)
-               }
+      bootstrap_iranges(r, L_s, L_b)
+    } else {
+      permute_blocks_iranges(r, L_s, L_b)
+    }
     r_prime <- GenomicRanges::sort(r_prime)
     # map the bootstrapped / permuted ranges back to chromosomes:
     x_prime <- map_line_to_chroms(r_prime, x)
@@ -48,20 +48,20 @@ bootstrap_granges <- function(x, type=c("bootstrap", "permute"), within_chrom=TR
 
 map_chroms_to_line <- function(x) {
   L_c <- unname(seqlengths(x))
-  chrom_shift <- c(0,cumsum(L_c)[-length(L_c)])
+  chrom_shift <- c(0, cumsum(L_c)[-length(L_c)])
   shift(ranges(x), rep(chrom_shift, seqnames(x)@lengths))
 }
 
 map_line_to_chroms <- function(r_prime, x) {
   L_c <- seqlengths(x)
-  chrom_shift <- c(0,cumsum(unname(L_c))[-length(L_c)])
-  chrom_blocks <- successiveIRanges(width=L_c)
+  chrom_shift <- c(0, cumsum(unname(L_c))[-length(L_c)])
+  chrom_blocks <- successiveIRanges(width = L_c)
   # need to deal with multiple overlaps...
-  idx <- findOverlaps(r_prime, chrom_blocks, select="first")
+  idx <- findOverlaps(r_prime, chrom_blocks, select = "first")
   r_on_chroms <- shift(r_prime, -chrom_shift[idx])
   chroms <- seqlevels(x)[idx]
   keep <- start(r_on_chroms) >= 1 & end(r_on_chroms) <= L_c[chroms]
-  GRanges(chroms[keep], r_on_chroms[keep], seqlengths=L_c)
+  GRanges(chroms[keep], r_on_chroms[keep], seqlengths = L_c)
 }
 
 #' Block bootstrap IRanges
@@ -73,8 +73,8 @@ map_line_to_chroms <- function(r_prime, x) {
 #' @export
 bootstrap_iranges <- function(x, L_s, L_b) {
   # first attempt: blocks go over L_s, and we will trim them later
-  n <- ceiling(L_s/L_b) 
-  random_blocks <- IRanges(start=round(runif(n, 1, (n-1)*L_b+1)), width=L_b)
+  n <- ceiling(L_s / L_b)
+  random_blocks <- IRanges(start = round(runif(n, 1, (n - 1) * L_b + 1)), width = L_b)
   rearranged_blocks <- successiveIRanges(width(random_blocks))
   block_shift <- start(rearranged_blocks) - start(random_blocks)
   fo <- findOverlaps(random_blocks, x)
@@ -91,9 +91,9 @@ bootstrap_iranges <- function(x, L_s, L_b) {
 #'
 #' @export
 permute_blocks_iranges <- function(x, L_s, L_b) {
-  n <- ceiling(L_s/L_b)
+  n <- ceiling(L_s / L_b)
   blocks <- successiveIRanges(rep(L_b, n))
-  mcols(x)$block <- findOverlaps(x, blocks, select="first")
+  mcols(x)$block <- findOverlaps(x, blocks, select = "first")
   perm <- sample(n)
   permuted_blocks <- successiveIRanges(width(blocks)[perm])
   permuted_blocks[perm] <- permuted_blocks
