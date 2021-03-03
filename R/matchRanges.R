7d86655ee3a7774c851ed32d7939d2b58ae91350
Eric Davis <ericscottdavis@outlook.com>
2021-03-03 14:56:29 -0500

    Update matchRanges for faster propensity score matching

--- R/matchRanges.R
+++ R/matchRanges.R
@@ -9,8 +9,7 @@
 #' @param x GRanges object containing focal ranges to match
 #' @param univ GRanges object containing the universe of ranges from which to select
 #' @param covar Character vector of covariates to match on
-#' @param method Character string passed to MatchIt used to calculate matches
-#'
+#' 
 #' @return Returns a GRanges object of matched controls
 #'
 #' @examples
@@ -20,36 +19,93 @@
 #'
 matchRanges <- function(x, univ, covar, method = "nearest") {
 
-  ## Add identifier for focal & universe groups
-  mcols(x)$mrid <- 1
-  mcols(univ)$mrid <- 0
-
-  ## Combine & convert to data.frame
-  df <- as.data.frame(c(x, univ))
-
-  ## Ensure covar columns are shared between x and univ
-  if (!(all(covar %in% colnames(mcols(x))) &
-    all(covar %in% colnames(mcols(univ))))) {
-    stop("covar must be columns in both x and univ.", call. = T)
-  }
-
+  ## Create data frame for matching covariates
+  df <- as.data.frame(cbind(id = factor(c(rep(1, length(x)), rep(0, length(univ)))),
+                            rbind(mcols(x)[covar], mcols(univ)[covar])))
+  
   ## Assemble covariate formula
-  f <- as.formula(paste("mrid ~", paste(covar, collapse = "+")))
+  f <- as.formula(paste("id ~", paste(covar, collapse = "+")))
+  
+  ## Run glm model
+  model <- glm(formula = f, data = df, family = binomial("logit"))
+  
+  ## Get x and univ propensity scores as vectors
+  ps_df <- data.frame(ps = predict(model, type = "link"), id = model$model$id)
+  xps <- ps_df[ps_df$id == 1,1]
+  ups <- ps_df[ps_df$id == 0,1]
+  
+  ## Create data table with original ups index and setkey to sort
+  dt <- data.table(ups, val = ups, index = 1:length(ups))
+  setkey(dt, ups)
+  
+  ## Find the ups index of the nearest neighbor to each value in xps with a rolling join
+  nnI <- dt[.(xps), index, roll = 'nearest', mult = 'first', with = T]
+  
+  ## Return the matched controls
+  return(univ[nnI])
 
-  ## Create matched control object
-  matchObj <- MatchIt::matchit(data = df, formula = f, method = method)
-
-  ## Extract matched data
-  md <- match.data(matchObj)
-
-  ## Convert to GRanges, filter for controls, remove Matchit columns, restore seqinfo
-  ctls <-
-    md %>%
-    plyranges::as_granges(keep_mcols = T) %>%
-    plyranges::filter(mrid == 0) %>%
-    plyranges::select(-c("mrid", "distance", "weights", "subclass")) %>%
-    `seqinfo<-`(value = seqinfo(univ))
-
-  ## Return matched controls
-  return(ctls)
 }
+
+# matchRangesOld2 <- function(x, univ, covar, method = "nearest") {
+#   
+#   ## Ensure covar columns are shared between x and univ
+#   if(!(all(covar %in% colnames(mcols(x))) & 
+#        all(covar %in% colnames(mcols(univ))))) {
+#     stop("covar must be columns in both x and univ.", call. = T)
+#   }
+#   
+#   ## Create data frame for matchit
+#   df <- as.data.frame(cbind(id = factor(c(rep(1, length(x)), rep(0, length(univ)))),
+#                             rbind(mcols(x)[covar], mcols(univ)[covar])))
+#   
+#   ## Assemble covariate formula
+#   f <- as.formula(paste("id ~", paste(covar, collapse = "+")))
+#   
+#   ## Create matched control object
+#   matchObj <- MatchIt::matchit(data = df, formula = f, method = method)
+#   
+#   ## Extract matched indicies
+#   matchIndex <- as.numeric(as.vector(matchObj$match.matrix))
+#   
+#   ## Return matchced controls
+#   return(c(x, univ)[matchIndex])
+#   
+# }
+
+# matchRangesOld <- function(x, univ, covar, method = "nearest") {
+# 
+#   ## Add identifier for focal & universe groups
+#   mcols(x)$mrid <- 1
+#   mcols(univ)$mrid <- 0
+# 
+#   ## Combine & convert to data.frame
+#   df <- as.data.frame(c(x, univ))
+# 
+#   ## Ensure covar columns are shared between x and univ
+#   if(!(all(covar %in% colnames(mcols(x))) &
+#        all(covar %in% colnames(mcols(univ))))) {
+#     stop("covar must be columns in both x and univ.", call. = T)
+#   }
+# 
+#   ## Assemble covariate formula
+#   f <- as.formula(paste("mrid ~", paste(covar, collapse = "+")))
+# 
+#   ## Create matched control object
+#   matchObj <- MatchIt::matchit(data = df, formula = f, method = method)
+# 
+#   ## Extract matched data
+#   md <- MatchIt::match.data(matchObj)
+# 
+#   ## Convert to GRanges, filter for controls, remove Matchit columns, restore seqinfo
+#   ctls <-
+#     md %>%
+#     plyranges::as_granges(keep_mcols = T) %>%
+#     plyranges::filter(mrid == 0) %>%
+#     plyranges::select(-c("mrid", "distance", "weights", "subclass")) %>%
+#     `seqinfo<-`(value = seqinfo(univ))
+# 
+#   ## Return matched controls
+#   return(ctls)
+# 
+# }
+
