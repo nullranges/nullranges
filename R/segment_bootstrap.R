b4574bbdf8c0528abadde7deaa7a666b67272f12
Wancen <42752661+Wancen@users.noreply.github.com>
2021-03-03 14:34:49 -0500

    resolve genes exceed chr boundary

--- R/segment_bootstrap.R
+++ R/segment_bootstrap.R
@@ -32,9 +32,10 @@ seg_bootstrap_granges <- function(seg, x, L_b, within_chrom=TRUE,proportion_leng
     L_s <- seg_length$Ls
     # store the gene digit for join_overlap_inner
     x$key.x<-seq_len(length(x)) # or check whether have this column in x
-    res<-seg_bootstrap_iranges(seg,x,L_s,L_b,chrom_lens)
+    
+    res<-seg_bootstrap_iranges(ranges(seg),x,seg$state,seqnames(seg),L_s,L_b,chrom_lens)
   }
-
+  
   return(res)
 }
 #' @export
@@ -56,8 +57,10 @@ seg_bootstrap_iranges_fast <- function(seg, x, state, L_c, L_s, L_b, proportion_
       p <- (width(seg2)) / sum(width(seg2))
       # number of blocks within each range of the segmentation
       times <- ceiling(L_c / L_b * p)
+      j<-which((end(seg2)-L_bs)<start(seg2))
+      width(seg2)[j]=L_bs
       # create random start positions within each segment
-      random_start <- unlist(mapply(function(time, x, y) runif(time, x, y), times, start(seg2), end(seg2)))
+      random_start <- unlist(mapply(function(time, x, y) runif(time, x, y), times, start(seg2), end(seg2)-L_bs+1))
       
       # shuffle the blocks
       random_start <- ifelse(length(random_start)==1,round(random_start),sample(random_start))
@@ -116,7 +119,7 @@ seg_bootstrap_iranges_fast <- function(seg, x, state, L_c, L_s, L_b, proportion_
 
 
 #' @export
-seg_bootstrap_iranges <- function(seg, x, L_s, L_b, chrom_lens) {  
+seg_bootstrap_iranges <- function(seg, x,state,chrnames,L_s, L_b, chrom_lens) { 
   L_c <- sum(chrom_lens)
 # the block width for each segmentation state is scaled
 # down to the segmentation state size, e.g. if segmentation state
@@ -127,42 +130,44 @@ seg_bootstrap_iranges <- function(seg, x, L_s, L_b, chrom_lens) {
     # the length of the block for this state
     L_bs <- L_b0[m]
     # the segmentation for this state
-    seg2 <- seg[seg$state == m]
+    seg2 <- seg[state == m]
     # fraction that each range of the segmentation comprises of the whole
     p <- (width(seg2)) / sum(width(seg2))
     # number of blocks within each range of the segmentation
     times <- ceiling(L_c / L_b * p)
-    seqnames = rep(as.character(seqnames(seg2)),times)
+    seqnames = rep(as.character(chrnames[state==m]),times)
+    j<-which((end(seg2)-L_bs)<start(seg2))
+    width(seg2)[j]=L_bs
     # create random start positions within each segment
-    random_start <- unlist(mapply(function(time, x, y) runif(time, x, y), times, start(seg2), end(seg2))) # hit issue negative "end(seg2)-L_bs"
+    random_start <- unlist(mapply(function(time, x, y) round(runif(time, x, y)), times, start(seg2), end(seg2)-L_bs+1)) # hit issue negative "end(seg2)-L_bs"
     # create the random blocks
     random_blocks_r <- IRanges(start = random_start, width = L_bs)
-    # random_blocks0 <- GRanges(seqnames = seqnames,ranges = random_blocks_r, seqlengths = chrom_lens, # warning message out-of-bound if not use "end(seg2)-L_bs"
-    #                         key.random = seq_len(length(random_start)))
-    random_blocks0 <- GRanges(seqnames = seqnames,ranges = random_blocks_r, seqlengths = chrom_lens) # warning message out-of-bound if not use "end(seg2)-L_bs"
+
     # the positions of the rearranged blocks in this segmentation state
     start_order <- unlist(mapply(function(x, y) seq(from = x, to = y, by = L_bs), start(seg2), end(seg2)))
-    rearranged_blocks0 <- IRanges(start = start_order, width = L_bs)
+    rearranged_blocks <- IRanges(start = start_order, width = L_bs)
   
     # shuffle the blocks
     index<-sample(length(random_start))
-    block_shift <- start(rearranged_blocks0)[index] - start(random_blocks_r)
-    return(list(random_blocks0,block_shift,index,seqnames))
+    block_shift <- start(rearranged_blocks)[index] - start(random_blocks_r)
+    return(list(random_blocks_r,block_shift,index,seqnames))
   })
   obj <- do.call(c,obj)
-  random_blocks <- do.call(c, obj[seq(1, length(obj), by = 4)])
-  random_blocks$key.random<-seq_len(length(random_blocks))
+  random_blocks_r <- do.call(c, obj[seq(1, length(obj), by = 4)])
   block_shift <- do.call(c, obj[seq(2, length(obj), by = 4)])
   index <- do.call(c, obj[seq(3, length(obj), by = 4)])
   seqnames <- do.call(c, obj[seq(4, length(obj), by = 4)])
+  random_blocks <- GRanges(seqnames = seqnames,ranges = random_blocks_r, seqlengths = chrom_lens) # warning message out-of-bound if not use "end(seg2)-L_bs"
+  random_blocks$key.random<-seq_len(length(random_blocks))
   
-  fo <- join_overlap_inner(x,random_blocks)
+  fo <- join_overlap_inner(x,random_blocks) # Question: do we want to only select once for each gene?
 
   x_prime <- IRanges::shift(ranges(x[fo$key.x]), block_shift[fo$key.random])
 
-  keep <- start(x_prime) >= 1 & end(x_prime) <= chrom_lens[seqnames[index[fo$key.random]]] # warning message out-of-bound
-  res<-GRanges(seqnames =seqnames[index[fo$key.random]][keep],ranges=x_prime[keep],seqlengths = chrom_lens)
-  
+  # keep <- start(x_prime) >= 1 & end(x_prime) <= chrom_lens[seqnames[index[fo$key.random]]] # fast but discard many genes hit boundary
+  gr_prime<-GRanges(seqnames =seqnames[index[fo$key.random]],ranges=x_prime)
+  chrom_blocks <- GRanges(seqnames = names(chrom_lens),ranges =IRanges(start=rep(1,length(chrom_lens)),width = chrom_lens)) #Question, do we want that as input
+  res<-join_overlap_intersect(gr_prime,chrom_blocks)
   return(res)
 }
 
