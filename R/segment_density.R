b74c02151c64805f6848152da014659221e5fb8a
Wancen <42752661+Wancen@users.noreply.github.com>
2021-04-30 11:02:16 -0400

    add plot argument for seg.density

--- R/segment_density.R
+++ R/segment_density.R
@@ -8,29 +8,22 @@
 #' use DNAcopy to segment) or "hmm" (which will use RcppHMM).
 #' The packages are not imported by nullranges, but must be installed
 #' by the user
-#' @param plot_origin plot the gene density of original gene Granges
+#' @param plot_segment plot the gene density of by segmentation
 #' @param boxplot boxplot of gene density for each state
 #'
 #' @import ggplot2
 #' @importFrom plyranges filter join_overlap_intersect
 #'
 #' @export
-segment_density <- function(x, n, Ls = 1e6, deny, type = c("cbs", "hmm"), plot_origin = TRUE, boxplot = FALSE) {
+segment_density <- function(x, n, Ls = 1e6, deny, type = c("cbs", "hmm"), plot_segment = TRUE, boxplot = FALSE) {
   query <- tileGenome(seqlengths(x)[seqnames(x)@values], tilewidth = Ls, cut.last.tile.in.chrom = TRUE)
   ## gap will create whole chromosome length ranges
   gap <- gaps(deny,end = seqlengths(x)) %>%
     plyranges::filter(strand=="*") 
   query_accept <- plyranges::join_overlap_intersect(query,gap2) %>% filter(width > Ls / 10)
   counts_nostand <- countOverlaps(query_accept, x)
-  counts <- counts_nostand/width(query_accept2)*Ls
+  counts <- counts_nostand/width(query_accept)*Ls
   eps <- rnorm(length(counts), 0, .2)
-
-  if (plot_origin) {
-    print(hist(counts, breaks = 50))
-    # a<-seqnames(query)
-    # b<-rep(a@values,a@lengths)
-    print(plot(sqrt(counts) + eps))
-  }
 
   if (type == "cbs") {
 
@@ -52,9 +45,6 @@ segment_density <- function(x, n, Ls = 1e6, deny, type = c("cbs", "hmm"), plot_o
     seq2 <- pmin(seq, q)
     km <- kmeans(seq2, n)
     mcols(query_accept)$states <- km$cluster
-    # TODO make optional to plot
-    plot(sqrt(counts) + eps, col = km$cluster)
-    
   } else if (type == "hmm") {
 
     if (!requireNamespace("RcppHMM", quietly=TRUE)) {
@@ -69,8 +59,11 @@ segment_density <- function(x, n, Ls = 1e6, deny, type = c("cbs", "hmm"), plot_o
       print = TRUE
     )
     v <- as.integer(factor(RcppHMM::viterbi(hmm, counts), levels = hmm$StateNames))
-    plot(sqrt(counts) + eps, col = v)
     mcols(query_accept)$states <- v
+  }
+  
+  if (plot_segment) {
+    plot(sqrt(counts) + eps, col = query_accept$states)
   }
   
   if (boxplot) {
