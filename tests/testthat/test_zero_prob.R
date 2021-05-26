f544b9a63a8124ff974cf88a2d76873375e6ce6c
mikelove <michaelisaiahlove@gmail.com>
2021-05-26 13:17:37 +0200

    adding test for zero prob

---
+++ tests/testthat/test_zero_prob.R
@@ -0,0 +1,31 @@
+library(nullranges)
+test_that("zero probability issue solved", {
+
+  suppressPackageStartupMessages(library(data.table))
+  set.seed(123)
+  dt <- data.table(
+    looped = c(rep(TRUE, 500), rep(FALSE, 10000)),
+    covar1 = c(rgamma(500, 2), abs(rnorm(10000, mean = 4, sd = 2))),
+    covar2 = c(sample(letters[1:5],
+                      size = 500,
+                      replace = TRUE,
+                      prob = c(0.1, 0.3, 0.4, 0.1, 0.05)),
+               sample(letters[1:5],
+                      size = 10000,
+                      replace = TRUE,
+                      prob = c(0.4, 0.3, 0.1, 0.1, 0.05)))
+  )
+
+  ## library(ggplot2)
+  ## ggplot(dt, aes(covar1, fill=looped)) +
+  ##   geom_histogram() +
+  ##   facet_wrap(~covar2)
+  
+  set.seed(123)
+  expect_error(matchRanges(focal = dt[dt$looped],
+                           pool = dt[!dt$looped],
+                           covar = ~covar1 + covar2,
+                           method = "rejection",
+                           replace = FALSE))
+
+})
