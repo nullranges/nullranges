1e62b6d1a6e516d15fb7b0cc25d0e7e78966d497
mikelove <michaelisaiahlove@gmail.com>
2023-02-06 13:25:06 -0500

    adding new function to help with segmenting

---
+++ R/bootHelper.R
@@ -0,0 +1,27 @@
+#' Segmentation based on one region
+#'
+#' This function makes a segmentation (GRanges) based on one
+#' region of one chromosome (seqnames).
+#'
+#' @param x a single region as GRanges object
+#'
+#' @return a segmentation (GRanges object) with the region of
+#' interest designated as state 2, and the rest of the chromosome
+#' as state 1.
+#' 
+#' @export
+oneRegionSegment <- function(x, seqlength) {
+  stopifnot(length(x) == 1)
+  chrom <- as.character(seqnames(x))
+  g <- genome(x)[[chrom]]
+  if (missing(seqlength) & !is.na(g)) {
+    si <- GenomeInfoDb::Seqinfo(genome=g)
+    seqlength <- seqlengths(si[chrom])[[1]]
+    } else {
+      stop("function requires seqlength argument or Seqinfo-supported genome")
+    }
+  GRanges(seqnames(x),
+          IRanges(c(1,start(x),end(x)+1),
+                  c(start(x)-1,end(x),seqlength)),
+          state=c(1,2,1))
+}
